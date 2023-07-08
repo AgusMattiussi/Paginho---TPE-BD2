48dd7f09f8f3e549e3f41744068ca0480a8c6f5f
Agustin Mattiussi <amattiussi@itba.edu.ar>
2023-07-08 17:26:43 -0300

    Add credentials validations to linkedAccounts routes

--- paginho/routesLinkedAccounts.py
+++ paginho/routesLinkedAccounts.py
@@ -12,7 +12,10 @@ router = APIRouter()
 
 # GET /linkedAccounts
 @router.get("/")
-async def get_linked_accounts(request: BasicAuthSchema, db: Session = Depends(get_db)):  
+async def get_linked_accounts(request: BasicAuthSchema, db: Session = Depends(get_db)): 
+    if not crud.validate_user(db, request.email, request.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+     
     try:
         linkedAccounts = crud.get_linked_entities(db, user=request)
         if linkedAccounts:
@@ -25,6 +28,9 @@ async def get_linked_accounts(request: BasicAuthSchema, db: Session = Depends(ge
 # POST /linkedAccounts
 @router.post("/")
 async def create_linked_account(request: LinkedAccountsPostSchema, db: Session = Depends(get_db)):
+    if not crud.validate_user(db, request.email, request.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+        
     if len(request.cbu) != CBU_LENGTH:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="CBU length must be 22 characters")
     
@@ -40,6 +46,9 @@ async def create_linked_account(request: LinkedAccountsPostSchema, db: Session =
 # PUT /linkedAccounts/{cbu}
 @router.put("/{cbu}")
 async def modify_linked_account(cbu: str, request: LinkedAccountsPutSchema, db: Session = Depends(get_db)):
+    if not crud.validate_user(db, request.email, request.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+
     if len(cbu) != CBU_LENGTH:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="CBU length must be 22 characters")
     
@@ -55,6 +64,9 @@ async def modify_linked_account(cbu: str, request: LinkedAccountsPutSchema, db: 
 # GET /linkedAccounts/{cbu}
 @router.get("/{cbu}")
 async def get_keys_for_linked_account(cbu: str, request: BasicAuthSchema, db: Session = Depends(get_db)):
+    if not crud.validate_user(db, request.email, request.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+    
     if len(cbu) != CBU_LENGTH:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="CBU length must be 22 characters")
     
