d347ebc53049ab8efcff0881dcc0ba252e996fb2
slarroude <slarroude@itba.edu.ar>
2023-07-08 19:31:59 -0300

    FEAT: Redis implementation ok, i guess

--- paginho/routesTransactions.py
+++ paginho/routesTransactions.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from schemas import PostTransactionSchema
 import crud
 from fastapi import APIRouter, HTTPException, status
+from redis_file import get_user_cbu_by_key
 
 router = APIRouter()
 
@@ -20,6 +21,9 @@ async def create_transaction(request: PostTransactionSchema, db: Session = Depen
     if not crud.validate_user(db, request.email, request.password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
     # TODO: Redis: request.key -> cbuTo
-    cbuTo = "2850590940090418135201"
+    cbuTo = get_user_cbu_by_key(request.key)
+    if not cbuTo:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Key not found")
+    # cbuTo = "2850590940090418135201"
     # TODO: Resolver transaccion con bancos
     return crud.create_transaction(db, cbuFrom=request.cbu, cbuTo=cbuTo, amount=request.amount)
