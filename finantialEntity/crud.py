580eab13fee8d26b25832109fd9679e1d66df090
Agustin Mattiussi <amattiussi@itba.edu.ar>
2023-07-08 18:37:56 -0300

    Finish implementing finantialEntityAPI endpoints

--- finantialEntity/crud.py
+++ finantialEntity/crud.py
@@ -1,12 +1,23 @@
 from sqlalchemy.orm import Session
 from pgDatabase import BankAccount, Transaction
 from datetime import datetime
+from decimal import Decimal
 
 
 # Accounts
 def get_account(cbu, db: Session):
     account = db.query(BankAccount).filter(BankAccount.cbu == cbu).first()
     return account
+
+def validate_account(cbu, db: Session):
+    return db.query(BankAccount).filter(BankAccount.cbu == cbu).count() > 0
+
+def modify_balance(cbu, amount, db: Session):
+    account = db.query(BankAccount).filter(BankAccount.cbu == cbu).first()
+    account.balance += Decimal(amount)
+    db.commit()
+    db.refresh(account)
+    return
 
 # Transactions
 def create_transaction(db: Session, cbuFrom: str, cbuTo: str, amount: float):
@@ -15,4 +26,8 @@ def create_transaction(db: Session, cbuFrom: str, cbuTo: str, amount: float):
     db.add(_transaction)
     db.commit()
     db.refresh(_transaction)
+
+    modify_balance(cbuFrom, -amount, db)
+    modify_balance(cbuTo, amount, db)
+
     return _transaction
--- finantialEntity/schemas.py
+++ finantialEntity/schemas.py
@@ -3,19 +3,28 @@ from pydantic import BaseModel
 
 
 class TransactionSchema(BaseModel): # POST /transactions
-    cbu_from: str = None
-    cbu_to: str = None
+    cbuFrom: str = None
+    cbuTo: str = None
     amount: float = None
     
     class Config:
         orm_mode = True
 
-class AccountDTO(BaseModel): # POST /users
+class AccountDTO(BaseModel):
     email: Optional[str] = None
     name: Optional[str] = None
     cuit: Optional[str] = None
     telephone: Optional[str] = None
     balance: Optional[float] = None
+
+    class Config:
+        orm_mode = True
+
+class TransactionDTO(BaseModel):
+    timestamp: Optional[str] = None
+    cbuFrom: Optional[str] = None
+    cbuTo: Optional[str] = None
+    amount: Optional[float] = None
 
     class Config:
         orm_mode = True
