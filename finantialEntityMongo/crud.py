5d581319a666a737753c28454f10edcc3d4af0fa
Agustin Mattiussi <amattiussi@itba.edu.ar>
2023-07-09 18:52:40 -0300

    Add multiple bank transaction and balance validation to financialEntityMongo API

--- finantialEntityMongo/crud.py
+++ finantialEntityMongo/crud.py
@@ -1,6 +1,10 @@
 from mongoDatabase import bankAccounts_serializer, Transaction, transactions_serializer, get_bankAccount_collection
 from datetime import datetime
 
+class NotEnoughFundsException(Exception):
+    """Raised when the account doesn't have enough funds to make the transaction"""
+    pass
+
 # Accounts
 def get_account(cbu, collection):
     accounts = bankAccounts_serializer(collection.find({"_id": cbu}))
@@ -10,19 +14,28 @@ def validate_account(cbu, collection):
     return collection.count_documents({"_id": cbu}) > 0
 
 def modify_balance(cbu, amount, collection):
-    update_operation = {"$inc": {"balance": amount}} if amount >= 0 else {"$inc": {"balance": amount}}
-    collection.update_one({"_id": cbu}, update_operation)
+    collection.update_one({"_id": cbu}, {"$inc": {"balance": amount}})
+
+def validate_transaction(cbu: str, amount: float, collection):
+    balance = bankAccounts_serializer(collection.find({"_id": cbu}))[0]['balance']
+    return balance >= amount
 
 # Transactions
-def create_transaction(collection, cbuFrom: str, cbuTo: str, amount: float):
+def create_transaction(collection, cbuFrom: str, cbuTo: str, amount: float, multiple_bank_transaction: bool):
+    bankAccountCollection = get_bankAccount_collection()
+
+    if not validate_transaction(cbuFrom, amount, bankAccountCollection):
+        raise NotEnoughFundsException()
+    
     formattedDatetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     _transaction = Transaction(time= formattedDatetime, cbuFrom=cbuFrom, cbuTo=cbuTo, amount=amount)
 
     _id = collection.insert_one(dict(_transaction))
     transaction = transactions_serializer(collection.find({"_id": _id.inserted_id}))
 
-    bankAccountCollection = get_bankAccount_collection()
-    modify_balance(cbuFrom, amount, bankAccountCollection)
-    modify_balance(cbuTo, amount, bankAccountCollection)
+    if not multiple_bank_transaction:
+        modify_balance(cbuTo, amount, bankAccountCollection)
+
+    modify_balance(cbuFrom, -amount, bankAccountCollection)
 
     return transaction
